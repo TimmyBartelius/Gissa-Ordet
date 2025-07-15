85a1e696f4c31c946cdb221037b67a76e5394779
Timmy <timmy.bartelius1@gmail.com>
2025-07-15 23:09:37 +0200

    Ändrat filen

--- deploy.js
+++ deploy.js
@@ -1,7 +1,13 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
+  branch: 'gh-pages',
   repo: 'https://github.com/TimmyBartelius/Gissa-Ordet.git',
+  user: {
+    name: 'Timmy Bartelius',
+    email: 'timmy.bartelius1@gmail.com'
+  },
+  message: 'Deploy via script'
 }, function (err) {
   if (err) {
     console.error('Deploy failed:', err);
