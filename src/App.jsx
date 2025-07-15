8c175a410ab660266d52c2794062388711afb850
Timmy <timmy.bartelius1@gmail.com>
2025-07-15 14:52:11 +0200

    Initierad + början på spellogik

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react';
+import {words} from './words';
+import './App.css';
+
+function App() {
+	const [currentWord, setCurrentWord] = useState('');
+	const [score, setScore] = useState(0);
+
+	const [gameStarted, setGameStarted] = useState(false);
+	const [timeLeft, setTimeLeft] = useState(0);
+	const [duration, setDuration] = useState(60);
+	const [gameOver, setGameOver] = useState(false);
+
+	const getRandomWord = () => {
+		const randomIndex = Math.floor(Math.random() * words.length);
+		return words[randomIndex];
+	};
+
+	const startGame = () =>{
+		setScore(0);
+		setTimeLeft(duration);
+		setCurrentWord(getRandomWord());
+		setGameStarted(true);
+		setGameOver(false);
+	};
+
+	const handleCorrectGuess = () =>{
+		setScore(prec => prev +1);
+		setCurrentWord(getRandomWord());
+	};
+
+	const handleDurationChange = (e) => {
+		setDuration(Number(e.target.value));
+	};
+
+	useEffect(()=>{
+		if (!gameStarted || timeLeft <= 0) return;
+		const interval = setInterval(()=>{
+			setTimeLeft(prev => {
+				if (prev <= 1){
+					clearInterval(interval);
+					setGameStarted(false);
+					setGameOver(true);
+					return 0;
+				}
+				return prev - 1;
+			});
+		}, 1000);
+		return () => clearInterval(interval);
+	}, [gameStarted, timeLeft]);
+	return (
+		<div>
+			<h1 className="title">Med Andra Ord</h1>
+
+			{!gameStarted && !gameOver && (<div className="time">
+			<label>Speltid: </label>
+			<select onChange={handleDurationChange} value={duration}>
+				<option value={30}>30 SEKUNDER</option>
+				<option value={60}>60 SEKUNDER</option>
+			</select>
+			<button className="startBtn" onClick={startGame}>Starta spelet!</button>
+			</div>
+		)}
+
+		{gameStarted && (
+			<div className="during-game">
+			<h2 className="word-display">{currentWord}</h2>
+			<p className="time-left">Tid kvar: {timeLeft} sek!</p>
+			<p className="current-score">Poäng: {score}</p>
+			<button className="correct-guess" onClick={handleCorrectGuess}>Rätt Gissat!</button>
+			</div>
+		)}
+
+		{gameOver && (
+			<div className="game-over">
+			<h2 className="time-end">Tiden är ute!</h2>
+			<h3 className="total-score">Total Poäng: {score}</h3>
+			<button className="next-game" onClick={startGame}>Spela igen</button>
+			</div>
+		)}
+
+		</div>
+	);
+}
+
+export default App
